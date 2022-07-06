33ef0f0ad8cc425b57a45e5283298dab52f9288c
susmitha-suresh <susmitha.suresh@perfomatix.com>
2022-07-06 14:19:29 +0530

    Changes

---
+++ src/main/java/com/sampleproject/springrabbitmqproject/SpringRabbitmqProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sampleproject.springrabbitmqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampleproject/springrabbitmqproject/SpringRabbitmqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleproject.springrabbitmqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
