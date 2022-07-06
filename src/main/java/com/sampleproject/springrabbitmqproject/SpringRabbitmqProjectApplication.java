2775af5dae6829fe9f44416bfd3308d36e5159d0
susmitha-suresh <susmitha.suresh@perfomatix.com>
2022-07-06 19:13:41 +0530

    Changes

--- src/main/java/com/sampleproject/springrabbitmqproject/SpringRabbitmqProjectApplication.java
+++ src/main/java/com/sampleproject/springrabbitmqproject/SpringRabbitmqProjectApplication.java
@@ -1,9 +1,11 @@
 package com.sampleproject.springrabbitmqproject;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringRabbitmqProjectApplication {
 
 	public static void main(String[] args) {
